06da32ef481e09a16ffe53c809439e0a5b9be45d
STS <megasts@yandex.ru>
2022-01-14 15:35:55 +0500

    Добавлен переключатель

--- lib/pizza_screen.dart
+++ lib/pizza_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:toggle_switch/toggle_switch.dart';
 
 class pizzaScreen extends StatefulWidget {
   const pizzaScreen({Key? key}) : super(key: key);
@@ -6,6 +7,7 @@ class pizzaScreen extends StatefulWidget {
   @override
   _pizzaScreenState createState() => _pizzaScreenState();
 }
+int _slimeDrouse = 0;
 
 class _pizzaScreenState extends State<pizzaScreen> {
   @override
@@ -16,7 +18,8 @@ class _pizzaScreenState extends State<pizzaScreen> {
         appBar: AppBar(toolbarHeight: 0,
         backgroundColor: Color.fromRGBO(253, 110, 1, 0.8),),
         body:
-        Column(
+        SingleChildScrollView(
+        child: Column(
           children: [
             SizedBox(height: 0),
             Row(
@@ -28,9 +31,9 @@ class _pizzaScreenState extends State<pizzaScreen> {
               ],
             ),
           Column(
-            children: const [
+            children: [
             SizedBox( height: 36),
-            SizedBox( height: 36,
+            const SizedBox( height: 36,
                 child: Text('Калькулятор пиццы', style:
               TextStyle (
                 //height: null,
@@ -43,7 +46,7 @@ class _pizzaScreenState extends State<pizzaScreen> {
               ),
               ),
               SizedBox( height: 9,),
-              SizedBox( height: 36,
+              const SizedBox( height: 36,
                 child: Text('Выберите параметры:', style:
                 TextStyle (
                   //height: null,
@@ -56,8 +59,37 @@ class _pizzaScreenState extends State<pizzaScreen> {
                 ),
               ),
               SizedBox( height: 33,),
+
+              ToggleSwitch(
+                changeOnTap: true,
+                minHeight: 36,
+                minWidth: 300.0,
+                animate: true,
+                animationDuration: 200,
+                cornerRadius: 36.0,
+                activeBgColors: const [[Color(0xFF0079D0)], [Color(0xFF0079D0)]],
+                activeFgColor: Colors.white,
+                inactiveBgColor: const Color(0xFFECEFF1),
+                inactiveFgColor: const Color.fromRGBO(0,0,0,0.4),
+                initialLabelIndex: _slimeDrouse,
+                totalSwitches: 2,
+                fontSize: 16.0,
+                labels: const ['Обычное тесто', 'Тонкое тесто'],
+                //customTextStyles: const [TextStyle(letterSpacing: -1.0),
+                //  TextStyle(letterSpacing: -1.0)],
+                radiusStyle: true,
+                onToggle: (index) {_slimeDrouse = index;
+                setState(() {_slimeDrouse = index;} );
+                },
+              ),
+              SizedBox( height: 33,),
           ]),
+
+
+
+
     ]),
+        ),
         //Text('Калькулятор пиццы'),
 
 
